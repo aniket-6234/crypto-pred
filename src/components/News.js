6e1f0c77aafdaa0c11d4c447479d45ce198944a6
aniket-raikwar <aniket@york.ie>
2024-02-27 18:50:11 +0530

    update news api

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ import axios from "axios";
 
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
-    newsCategory: "science_and_technology",
+    newsCategory: "Cryptocurrency",
     count: simplified ? 6 : 35,
   });
 
@@ -16,7 +16,7 @@ const News = ({ simplified }) => {
     scrollToTopAfterPageRender();
   }, []);
 
-  if (!cryptoNews?.length > 0) {
+  if (!cryptoNews?.data?.length > 0) {
     return (
       <div className="flex bg-black w-screen h-screen justify-center items-center">
         <img className="w-[80px]" src={Loader} alt="loader" />
@@ -26,58 +26,105 @@ const News = ({ simplified }) => {
 
   return (
     <div className="px-4 lg:px-24 bg-black overflow-auto">
-      {!simplified && (
-        <div className="pb-10">
-          <h2 className="text-md lg:text-2xl font-md mt-20 lg:mt-24 text-white page-text">
-            News
-          </h2>
-          <div className="w-full h-[0.5px] mt-2 lg:mt-2 bg-[#313131]"></div>
+      {!simplified ? (
+        <div>
+          <div className="pb-10">
+            <h2 className="text-md lg:text-2xl font-md mt-20 lg:mt-24 text-white page-text">
+              News
+            </h2>
+            <div className="w-full h-[0.5px] mt-2 lg:mt-2 bg-[#313131]"></div>
+          </div>
+          <div className="news-container">
+            {cryptoNews?.data?.map((news, i) => (
+              <div key={i} className="news-card">
+                <a
+                  href={news.link}
+                  target="_blank"
+                  rel="norefferer"
+                  className="news-box"
+                >
+                  <div className="flex justify-between">
+                    <h3 className="news-head">
+                      {news.title.length >= 60
+                        ? news.title.substring(0, 60) + "..."
+                        : news.title}{" "}
+                    </h3>
+                  </div>
+                  <p className="news-para">
+                    {news.title.length >= 100
+                      ? news.title.substring(0, 100) + "..."
+                      : news.title}
+                  </p>
+                  <div className=" flex justify-between items-center news-bottom">
+                    <div className="flex justify-between">
+                      <div className="w-10 h-10 rounded-full">
+                        <img
+                          className="w-8 h-8 rounded-full"
+                          src={news?.source_favicon_url || DemoNewsImage}
+                          alt="icon"
+                        />
+                      </div>
+                      <p className="text-xs ml-1 mt-2 text-[#ffffff]">
+                        {news?.sourceName}
+                      </p>
+                    </div>
+                    <div className="news-time-div">
+                      <p className="news-time-text">
+                        {moment(news?.publishedAt).format("Do MMMM, YYYY")}
+                      </p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="news-container">
+          {cryptoNews?.data?.slice(0, 6).map((news, i) => (
+            <div key={i} className="news-card">
+              <a
+                href={news.link}
+                target="_blank"
+                rel="norefferer"
+                className="news-box"
+              >
+                <div className="flex justify-between">
+                  <h3 className="news-head">
+                    {news.title.length >= 60
+                      ? news.title.substring(0, 60) + "..."
+                      : news.title}{" "}
+                  </h3>
+                </div>
+                <p className="news-para">
+                  {news.title.length >= 100
+                    ? news.title.substring(0, 100) + "..."
+                    : news.title}
+                </p>
+                <div className=" flex justify-between items-center news-bottom">
+                  <div className="flex justify-between">
+                    <div className="w-10 h-10 rounded-full">
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src={news?.source_favicon_url || DemoNewsImage}
+                        alt="icon"
+                      />
+                    </div>
+                    <p className="text-xs ml-1 mt-2 text-[#ffffff]">
+                      {news?.sourceName}
+                    </p>
+                  </div>
+                  <div className="news-time-div">
+                    <p className="news-time-text">
+                      {moment(news?.publishedAt).format("Do MMMM, YYYY")}
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          ))}
         </div>
       )}
-      <div className="news-container">
-        {cryptoNews?.map((news, i) => (
-          <div key={i} className="news-card">
-            <a
-              href={news.link}
-              target="_blank"
-              rel="norefferer"
-              className="news-box"
-            >
-              <div className="flex justify-between">
-                <h3 className="news-head">
-                  {news.title.length >= 60
-                    ? news.title.substring(0, 60) + "..."
-                    : news.title}{" "}
-                </h3>
-              </div>
-              <p className="news-para">
-                {news.title.length >= 100
-                  ? news.title.substring(0, 100) + "..."
-                  : news.title}
-              </p>
-              <div className=" flex justify-between items-center news-bottom">
-                <div className="flex justify-between">
-                  <div className="w-10 h-10 rounded-full">
-                    <img
-                      className="w-8 h-8 rounded-full"
-                      src={news?.image || DemoNewsImage}
-                      alt="icon"
-                    />
-                  </div>
-                  <p className="text-xs ml-1 mt-2 text-[#ffffff]">
-                    {news?.sourceName}
-                  </p>
-                </div>
-                <div className="news-time-div">
-                  <p className="news-time-text">
-                    {moment(news?.publishedAt).format("Do MMMM, YYYY")}
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
--- src/redux/services/cryptoNewsApi.js
+++ src/redux/services/cryptoNewsApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeader = {
-  "x-rapidapi-host": "newsi-api.p.rapidapi.com",
+  "x-rapidapi-host": "real-time-news-data.p.rapidapi.com",
   "x-rapidapi-key": "21d65a5c87mshd7a56e79ec0552ap1988a3jsn0c5fe959e062",
 };
 
-const baseUrl = "https://newsi-api.p.rapidapi.com/api/category";
+const baseUrl = "https://real-time-news-data.p.rapidapi.com/search";
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeader });
 
@@ -16,7 +16,7 @@ export const cryptoNewsApi = createApi({
     getCryptoNews: builder.query({
       query: ({ newsCategory, count }) =>
         createRequest(
-          `?category=${newsCategory}&language=en&country=us&sort=top&page=1&limit=${count}`
+          `?query=${newsCategory}&lang=en&country=US`
         ),
     }),
   }),
